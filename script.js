9bcb9f0ca5515a78a5352fbe7c36cd2aef719f29
Miracle2135L <ankitpatel190620@gmail.com>
2025-03-12 22:01:01 +0530

    added score system

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let rock = document.getElementById("rock");
 let paper = document.getElementById("paper");
 let sissor = document.getElementById("sissor");
 let showResult = document.getElementById("result");
+let pScore = document.getElementById("pscore");
+let bScore = document.getElementById("bscore");
 
 let choices = ["rock","paper","sissor"];
 let result;
@@ -9,21 +11,30 @@ let result;
 let userChoice;
 let botChoice;
 
+let botScore = 0;
+let playerScore = 0;
+
 let choose = (e) => {
     botChoice = choices[Math.floor(Math.random() * 3)];
     userChoice = e.target.id;
     if (userChoice == "rock" && botChoice == "paper") {
         result = "you lose";
+        botScore++;
     } else if (userChoice == "paper" && botChoice == "sissor"){
         result = "you lose";
+        botScore++;
     } else if (userChoice == "sissor" && botChoice == "rock"){
         result = "you lose";
+        botScore++;
     } else if (userChoice == botChoice) {
         result = "it's draw";
     }else{
         result = "you won";
+        playerScore++;
     }
-    showResult.innerText = result + ", bot choosed : " + botChoice;
+    showResult.innerText = "result: " + result + ", bot choosed " + botChoice;
+    pScore.innerText = "your score: " + playerScore;
+    bScore.innerText = "computer's score: " + botScore;
 }
 
 rock.onclick = choose;
